de4ef7c00a4c77ac26fcd8f82ee52513a10d6296
deviant <48874057+DeviantSec@users.noreply.github.com>
2021-10-20 22:31:24 -0700

    Update cleanup.sh
    Adding additional cleanups

--- Cleanup/cleanup.sh
+++ Cleanup/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Just a simple script to clean up and make life a little easier :D
-# I  use this on client machines to ensure that if it is not decommissioned after the penetration test that evidence is minimal.
+# Author: Deviant
 
 # Delete bash history
 echo "" > /root/.bash_history
@@ -8,26 +8,31 @@ echo "" > ~/.bash_history
 
 # Delete Metasploit l00ts
 echo "" > /root/.msf4/history
-rm -r /root/.msf4/loot/*
-rm -r /root/.msf4/logs/*
+rm -rf /root/.msf4/loot/*
+rm -rf /root/.msf4/logs/*
+rm -rf /root/.msf4/local/*
 
 echo "" > ~/.msf4/history
-rm -r ~/.msf4/loot/*
-rm -r ~/.msf4/logs/*
+rm -rf ~/.msf4/loot/*
+rm -rf ~/.msf4/logs/*
+rm -rf ~/.msf4/local/*
 
 # Delete John files
-rm -r /root/.john/*
-rm -r ~/.john/*
+rm -rf /root/.john/*
+rm -rf ~/.john/*
 
 # Delete Hashcat dfiles
-rm -r /root/.hashcat/*.potfile
-rm -r /root/.hashcat/sessions
-rm -r ~/.hashcat/*.potfile
-rm -r ~/.hashcat/sessions
+rm -rf /root/.hashcat/*.potfile
+rm -rf /root/.hashcat/sessions
+rm -rf ~/.hashcat/*.potfile
+rm -rf ~/.hashcat/sessions
 
-# Miscellanious cleanups 
-rm -r /opt/tools
+# Miscellanious cleanups
 rm -rf /root/tools
-rm -r /tmp/tmp
+rm -rf ~/tools
+rm -rf /tmp/*
 rm -rf /opt/nessus
 rm -rf /root/evidence
+rm -rf ~/evidence
+rm -rf /root/.cme
+rm -rf ~/.cme
